00e48e2af0240f8eef14867cdf7387761ad361e6
Joshua <koffworld@gmail.com>
2023-06-19 14:54:41 +0000

    minor changes

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -14,14 +14,14 @@ const Products = async () => {
   return (
     <div className='min-h-screen bg-[#f1f1f1]'>
         <Navbar />
-        <div className='md:p-24 sm:p-8 mt-[3rem] flex gap-6 justify-center flex-wrap w-full'>
+        <div className='md:p-24 sm:p-8 mt-[3rem] flex gap-6 justify-center flex-wrap w-full product-card'>
             {
                 products.map((product) => (
                     <div className='bg-white rounded-lg p-8 w-[90%] md:w-[350px] flex flex-col items-center gap-3 hover:shadow-2xl cursor-pointer'>
                         <img src={product.image} alt="product image" className='h-[25vh]'/>
                         <div>
                             <h1 className='mt-3 mb-3 font-semibold text-lg text-[#242424]'>{product.title}</h1>
-                            <p className='text-sm text-[#818181]'>{(product.category).toUpperCase()}</p>
+                            <p className='text-sm text-[#818181] mb-3'>{(product.category).toUpperCase()}</p>
                             <p className='text-[#818181]'>{shortenText(product.description, 70)}</p>
                         </div>
                     </div>
