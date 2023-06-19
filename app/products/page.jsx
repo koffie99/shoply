b6e3e2bfae3b2af57ae80dfbe8d949509c8669a3
Joshua <koffworld@gmail.com>
2023-06-19 14:20:08 +0000

    minor changes

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -18,7 +18,7 @@ const Products = async () => {
         <div className='p-24 flex gap-6 justify-center flex-wrap w-full'>
             {
                 products.map((product) => (
-                    <div className='bg-white rounded-lg p-8 w-[350px] flex flex-col items-center gap-3 hover:shadow-2xl cursor-pointer'>
+                    <div className='bg-white rounded-lg p-8 w-[90%] md:w-[350px] flex flex-col items-center gap-3 hover:shadow-2xl cursor-pointer'>
                         <img src={product.image} alt="product image" className='h-[25vh]'/>
                         <div>
                             <h1 className='mt-3 mb-3 font-semibold text-lg'>{product.title}</h1>
