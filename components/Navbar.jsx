a77f8ade34bf5ac4c747dd80550d562c53527739
Joshua <koffworld@gmail.com>
2023-06-19 14:40:32 +0000

    minor changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
   return (
-    <div className='h-[10vh] bg-white flex items-center justify-between md:px-24 sm:px-10 fixed top-0 w-full nav'>
+    <div className='h-[10vh] bg-white flex items-center justify-between md:px-24 sm:px-10 shadow-lg w-full nav'>
         <div><h1 className='font-semibold mb-8 text-lg text-[mediumseagreen]'>Shoply</h1></div>
         <div>
             {/* <ShoppingCartIcon /> */}
