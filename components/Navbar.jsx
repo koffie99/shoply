ed23862e079d7807e5d71cdd44999cb86c4f7a87
Joshua <koffworld@gmail.com>
2023-06-19 14:49:43 +0000

    minor changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className='h-[10vh] bg-white flex items-center justify-between px-15 shadow-lg w-full nav'>
-        <Link href="/" className='font-semibold text-lg text-[mediumseagreen]'>Shoply</Link>
-        <p>Welcome, User</p>
+        <Link href="/" className='font-semibold pl-10 pr-10 text-lg text-[mediumseagreen]'>Shoply</Link>
+        <p className='text-[#242424] pl-10 pr-10'>Welcome, User</p>
     </div>
   )
 }
